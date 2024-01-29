a3cd43200fd0eeac5d963279db295fb40c7e0a43
Damn <35892166+Mikorsa@users.noreply.github.com>
2024-01-29 09:27:04 +0000

    Started working on it :3

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("comissionsButton").onclick = function () {
+    location.href = "comissions.html";
+};
+document.getElementById("libraryButton").onclick = function () {
+    location.href = "library.html";
+};
+document.getElementById("socialButton").onclick = function () {
+    location.href = "social.html";
+};
